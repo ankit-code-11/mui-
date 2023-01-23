5c8d3c3d1c5689989a8cb8ae325d5d76fa6395ba
Pradeep Kumar <62685521+PKJ607@users.noreply.github.com>
2023-01-23 19:03:10 +0530

    install material ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { addToCart } from './redux/action';
 import { useDispatch } from 'react-redux';
+import Header from './components/Header';
 
 function App() {
   const dispatch = useDispatch();
@@ -11,6 +12,7 @@ function App() {
   };
   return (
     <div className="App" >
+      <Header />
       <button onClick={() => dispatch(addToCart(product))} >
         Add to Cart
       </button>
